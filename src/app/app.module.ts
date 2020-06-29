9d120fb311cb2db257e0a87800e805069d7ff79c
marek.kita <marek.kita@accenture.com>
2020-06-29 15:26:05 +0200

    update to REACTIVEFORMSMODULE DIRECTIVE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customers/customer.component';
@@ -14,7 +14,7 @@ import { CustomerReactiveComponent } from './customersReactive/customer-reactive
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    ReactiveFormsModule
   ],
   bootstrap: [AppComponent]
 })
