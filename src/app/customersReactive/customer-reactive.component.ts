02e6a3b6882c8343c30c8aa357d13d51cf1ff7e3
marek.kita <marek.kita@accenture.com>
2020-07-06 14:49:38 +0200

    CROSS-FIELD VALIDATION: CUSTOM VALIDATOR

--- src/app/customersReactive/customer-reactive.component.ts
+++ src/app/customersReactive/customer-reactive.component.ts
@@ -3,15 +3,29 @@ import {AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators} from "
 
 import {Customer} from './customer';
 
-function ratingRange(min: number, max:number): ValidatorFn {
+function ratingRange(min: number, max: number): ValidatorFn {
   return (c: AbstractControl): { [key: string]: boolean } | null => {
     if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
-      return { 'range': true };
+      return {'range': true};
     }
     return null;
   }
 }
 
+function emailMatcher(c: AbstractControl): { [key: string]: boolean } | null {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+
+  if (emailControl.value === confirmControl.value) {
+    return null;
+  }
+
+  return {'match': true};
+}
 
 @Component({
   selector: 'app-customer-reactive',
@@ -22,19 +36,20 @@ export class CustomerReactiveComponent implements OnInit {
   customerForm: FormGroup;
   customer = new Customer();
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit() {
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: [{value: '', disabled: false}, [Validators.required, Validators.maxLength(50)]],
       emailGroup: this.fb.group({
-        email: ['',[Validators.required, Validators.email]],
-        confirmEmail: ['',[Validators.required]],
-      }),
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', [Validators.required]]
+      }, {validator: emailMatcher}),
       phone: '',
       notification: 'email',
-      rating: [null, ratingRange(1,3)],
+      rating: [null, ratingRange(1, 3)],
       sendCatalog: [{value: true, disabled: true}],
     })
   }
@@ -62,6 +77,7 @@ export class CustomerReactiveComponent implements OnInit {
       sendCatalog: false
     })
   }
+
   populateTestDataPatchValue(): void {
     this.customerForm.patchValue({
       lastName: 'Smith'
