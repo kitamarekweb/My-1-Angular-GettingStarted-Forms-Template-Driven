53e03352ada01ef5bde95527265858a471c13641
marek.kita <marek.kita@accenture.com>
2020-07-06 11:27:36 +0200

    FormsModule for Template driven forms is need

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+// import { FormsModule } from "@angular/forms"; // FormsModule is for template-driven forms
 
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customers/customer.component';
@@ -14,7 +15,8 @@ import { CustomerReactiveComponent } from './customersReactive/customer-reactive
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // FormsModule // FormsModule is for template-driven forms
   ],
   bootstrap: [AppComponent]
 })
