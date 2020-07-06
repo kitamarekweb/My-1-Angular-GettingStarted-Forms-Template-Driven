8845b0e82034d5e714125109451ef2ecc01ac9d6
marek.kita <marek.kita@accenture.com>
2020-07-06 12:55:23 +0200

    CUSTOM VALIDATOR, create FUNCTION for validation

--- src/app/customersReactive/customer-reactive.component.ts
+++ src/app/customersReactive/customer-reactive.component.ts
@@ -1,7 +1,15 @@
 import {Component, OnInit} from '@angular/core';
-import {FormGroup, FormControl, FormBuilder, Validators} from "@angular/forms";
+import {FormGroup, FormControl, FormBuilder, Validators, AbstractControl} from "@angular/forms";
 
 import {Customer} from './customer';
+import {stringify} from "querystring";
+
+function ratingRange(c: AbstractControl): { [key: string]: boolean } | null {
+  if (c.value !== null && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
+    return { 'range': true };
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-customer-reactive',
@@ -21,6 +29,7 @@ export class CustomerReactiveComponent implements OnInit {
       email: ['',[Validators.required, Validators.email]],
       phone: '',
       notification: 'email',
+      rating: [null, ratingRange],
       sendCatalog: [{value: true, disabled: true}],
     })
   }
